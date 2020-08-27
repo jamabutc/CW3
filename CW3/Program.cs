ae3a320f38f2bbb5f1c330af10487eb7203170a9
jamabutc <jamabutc@ut.utm.edu>
2020-08-27 18:05:27 -0500

    Coded Hello World in an emply console project.

--- CW3/Program.cs
+++ CW3/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace CW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
