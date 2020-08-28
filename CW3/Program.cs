8ecc6e021e439d15c5bf2c35535ed7fda1edc5a5
jamabutc <jamabutc@ut.utm.edu>
2020-08-28 18:53:10 -0500

    Program modified to include number guessing game.

--- CW3/Program.cs
+++ CW3/Program.cs
@@ -8,38 +8,33 @@ namespace CW3
         {
             Console.WriteLine("Hello World!");
 
+            int randomNum = newNum(1, 101);
+            int count = 1;
             while (true)
             {
-                int randomNum = newNum(1, 101);
-                int count = 1;
-                while (true)
+                Console.Write("I am thinking of a number between 0 and 100:");
+                int input = Convert.ToInt32(Console.ReadLine());
+                
+                if (input < randomNum)
                 {
-                    Console.Write("Enter a number between 1 and 100 or enter 0 to quit):");
-                    int input = Convert.ToInt32(Console.ReadLine());
-                    if (input == 0)
-                        return;
-                    else if (input < randomNum)
-                    {
-                        Console.WriteLine("Low, try again.");
-                        ++count;
-                        continue;
-                    }
-                    else if (input > randomNum)
-                    {
-                        Console.WriteLine("High, try again.");
-                        ++count;
-                        continue;
-                    }
-                    else
-                    {
-                        Console.WriteLine("You guessed it! The number was {0}!", randomNum);
-                        Console.WriteLine("It took you {0} {1}.\n", count,
-                                          count == 1 ? "try" : "tries");
-                        break;
-                    }
+                    Console.WriteLine("Low, try again.");
+                    ++count;
+                    continue;
+                }
+                else if (input > randomNum)
+                {
+                    Console.WriteLine("High, try again.");
+                    ++count;
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it! The number was {0}!", randomNum);
+                    Console.WriteLine("It took you {0} {1}.\n", count,
+                                      count == 1 ? "try" : "tries");
+                    break;
                 }
             }
-
         }
         static int newNum(int min, int max)
         {
@@ -48,3 +43,4 @@ namespace CW3
         }
     }
 }
+
